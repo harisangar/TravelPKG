c6676a76970f080aaba51075e1d0b51b43e1fb5f
harisangar <ganambihai10@gmail.com>
2023-10-16 20:30:07 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-red-900">
+     <h1 className='text-green-500'>hello from app</h1>
+    </div>
+  );
+}
+
+export default App;
