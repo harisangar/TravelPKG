6f7e6590050e4aa51614929b91739542d7c7161b
harisangar <ganambihai10@gmail.com>
2023-10-25 07:41:36 +0530

    theme explore creation

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,75 +4,115 @@ import 'react-alice-carousel/lib/alice-carousel.css';
 import Explore from '../shared/explore';
 import { img1, img2, img3, img4, img5 } from '../../img/index';
 import ThemeExplore from '../ThemeExplore/ThemeExplore';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
 
 function Home() {
 
   const handleDragStart = (e) => e.preventDefault();
 
+  // const items = [
+  //   <img
+  //     className=' h-[500px] w-full'
+  //     src={img1}
+  //     alt='img'
+  //     onDragStart={handleDragStart}
+  //     role='presentation'
+  //   />,
+  //   <img
+  //     className='h-[500px]  w-full'
+  //     src={img2}
+  //     alt='img'
+  //     onDragStart={handleDragStart}
+  //     role='presentation'
+  //   />,
+  //   <img
+  //     className=' h-[500px] w-full'
+  //     src={img3}
+  //     alt='img'
+  //     onDragStart={handleDragStart}
+  //     role='presentation'
+  //   />,
+  //   <img
+  //     className='h-[500px] w-full'
+  //     src={img4}
+  //     alt='img'
+  //     onDragStart={handleDragStart}
+  //     role='presentation'
+  //   />,
+  //   <img
+  //     className='  h-[500px] w-full'
+  //     src={img5}
+  //     alt='img'
+  //     onDragStart={handleDragStart}
+  //     role='presentation'
+  //   />,
+  // ];
+
   const items = [
-    <img
-      className=' h-[500px] w-full'
-      src={img1}
-      alt='img'
-      onDragStart={handleDragStart}
-      role='presentation'
-    />,
-    <img
-      className='h-[500px]  w-full'
-      src={img2}
-      alt='img'
-      onDragStart={handleDragStart}
-      role='presentation'
-    />,
-    <img
-      className=' h-[500px] w-full'
-      src={img3}
-      alt='img'
-      onDragStart={handleDragStart}
-      role='presentation'
-    />,
-    <img
-      className='h-[500px] w-full'
-      src={img4}
-      alt='img'
-      onDragStart={handleDragStart}
-      role='presentation'
-    />,
-    <img
-      className='  h-[500px] w-full'
-      src={img5}
-      alt='img'
-      onDragStart={handleDragStart}
-      role='presentation'
-    />,
+    {image : img1},
+    {image : img2},
+    {image : img3}
   ];
+
+   const settings = {
+     dots: true,
+     infinite: true,
+     speed: 500,
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     autoplay: true,
+     autoplaySpeed: 2000,
+   };
   return (
     <>
-      <div className='flex flex-col items-center mt-2'>
-        <h1 className='font-bold text-xl'>Customize & Book</h1>
-        <h1 className='font-bold text-xl'>Amazing Holiday Packages</h1>
+      <div className='relative'>
+        <div className=' px-10 py-10 mt-2 bg-purple-200 mx-3 rounded-lg realtive'>
+          <Slider {...settings}>
+            {items.map((item, index) => (
+              <img
+                className='h-[450px] rounded-lg shadow'
+                src={item.image}
+                alt='img'
+              />
+            ))}
+          </Slider>
+        </div>
+        <div className='absolute top-[300px] text-white left-[450px]'>
+          <div className=' flex flex-col items-center mt-5'>
+            <h1 className='font-bold text-6xl '>Customize & Book</h1>
+            <h1 className='font-bold text-3xl'>Amazing Holiday Packages</h1>
+            <h1 className='font-light'>
+              650+ Travel Agents serving 65+ Destinations worldwide
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div className=' flex flex-col items-center mt-5'>
+        <h1 className='font-bold text-5xl '>Customize & Book</h1>
+        <h1 className='font-bold text-3xl'>Amazing Holiday Packages</h1>
         <h1 className='font-light'>
           650+ Travel Agents serving 65+ Destinations worldwide
         </h1>
       </div>
 
-      <div className='  p-10 r bg-white realtive'>
-        <AliceCarousel
-          // style={{ backgroundColor: 'red', padding: '10px' }}
-          autoPlay={true}
-          infinite={true}
-          disableSlideInfo={true}
-          disableDotsControls={true}
-          disableButtonsControls={true}
-          animationDuration={1000}
-          mouseTracking
-          items={items}
-        />
-      </div>
       <div className='flex justify-center m-5'>
         <Explore />
       </div>
-      <div className='mx-20 '>
+      <div className='bg-gray-300 mb-3 mx-3 rounded-lg p-5 flex flex-col justify-center  font-semibold'>
+        <div className='flex justify-between px-40 items-center'>
+          <h1 className='text-2xl text-purple-800'>
+            Explore Packages By Themes
+          </h1>
+          <h1 className='text-sm'>
+            <span className='mx-1'>
+              <i class='ri-phone-line'></i>
+            </span>
+            For best packages, call us at 1800-123-5555
+          </h1>
+        </div>
+
         <ThemeExplore />
       </div>
     </>
--- src/Components/ThemeExplore/ThemeExplore.js
+++ src/Components/ThemeExplore/ThemeExplore.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import ReactCardSlider from 'react-card-slider-component';
+import React from 'react';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import {
   adventure,
   beaches,
@@ -11,85 +12,98 @@ import {
   wateractivities,
   wildlife,
 } from '../../img/themes/index';
+import Slider from 'react-slick';
 
 const ThemeExplore = () => {
-
-    const sliderClick = ()=>{
-        console.log('clicked')
-    }
-    const slides = [
-      {
-        image: adventure ,
-        title: 'Adventure',
-        description: 'adventure Theme',
-        clickEvent: sliderClick,
-      },
-      {
-        image:  beaches ,
-        title: 'Seasonal',
-        description: 'Seasonal Theme',
-        clickEvent: sliderClick,
-      },
-      {
-        image:  family ,
-        title: 'Family',
-        description: 'Family Theme',
-        clickEvent: sliderClick,
-      },
-      {
-        image: nature ,
-        title: 'Nature',
-        description: 'Nature Theme',
-        clickEvent: sliderClick,
-      },
-      {
-        image: religious ,
-        title: 'Religious',
-        description: 'Relegious Theme',
-        clickEvent: sliderClick,
-      },
-      {
-        image:  friends_group ,
-        title: 'Friends',
-        description: 'Friends Theme',
-        clickEvent: sliderClick,
-      },
-      {
-        image: honeymoon ,
-        title: 'Honeymoon',
-        description: 'Honeymoon Theme',
-        clickEvent: sliderClick,
-      },
-      {
-        image:  wateractivities ,
-        title: 'Water Activities',
-        description: 'Water Activities',
-        clickEvent: sliderClick,
-      },
-      {
-        image:  wildlife ,
-        title: 'Wild Life',
-        description: 'Wildlife Theme',
-        clickEvent: sliderClick,
-      },
-    ];
+  const sliderClick = () => {
+    console.log('clicked');
+  };
+  const slides = [
+    {
+      image: adventure,
+      title: 'Adventure',
+      description: 'adventure Theme',
+      clickEvent: sliderClick,
+    },
+    {
+      image: beaches,
+      title: 'Seasonal',
+      description: 'Seasonal Theme',
+      clickEvent: sliderClick,
+    },
+    {
+      image: family,
+      title: 'Family',
+      description: 'Family Theme',
+      clickEvent: sliderClick,
+    },
+    {
+      image: nature,
+      title: 'Nature',
+      description: 'Nature Theme',
+      clickEvent: sliderClick,
+    },
+    {
+      image: religious,
+      title: 'Religious',
+      description: 'Relegious Theme',
+      clickEvent: sliderClick,
+    },
+    {
+      image: friends_group,
+      title: 'Friends',
+      description: 'Friends Theme',
+      clickEvent: sliderClick,
+    },
+    {
+      image: honeymoon,
+      title: 'Honeymoon',
+      description: 'Honeymoon Theme',
+      clickEvent: sliderClick,
+    },
+    {
+      image: wateractivities,
+      title: 'Water Activities',
+      description: 'Water Activities',
+      clickEvent: sliderClick,
+    },
+    {
+      image: wildlife,
+      title: 'Wild Life',
+      description: 'Wildlife Theme',
+      clickEvent: sliderClick,
+    },
+  ];
+  const settings={
+    dots:true,
+    infinite:true,
+    speed:500,
+    slidesToShow:5,
+    slidesToScroll:1
+  };
   return (
-    <div className='bg-purple-400 p-5'>
-      <ReactCardSlider slides={slides} className=''>
-        {slides.map((slide, index) => (
-          <div className='h-20' key={index}>
-            <img
-              src={slide.image}
-              alt={slide.title}
-              className=''
-            />
-            <h3>{slide.title}</h3>
-            <p>{slide.description}</p>
-          </div>
-        ))}
-      </ReactCardSlider>
+    <div className='w-3/4 m-auto'>
+      <div className='mt-5 mb-3'>
+        <Slider {...settings}>
+          {slides.map((d, index) => (
+            <div className='bg-white h-auto text-black rounded-xl'>
+              <div className='h-56 rounded-t-xl bg-purple-400 flex justify-center items-center'>
+                <img src={d.image} alt='' className='h-44 rounded-full' />
+              </div>
+
+              <div className='flex flex-col justify-center items-center gap-4 p-4'>
+                <p className='text-md font-semibold'>{d.title}</p>
+                <p>{d.description}</p>
+                
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+
+      
     </div>
   );
-}
+};
 
-export default ThemeExplore
+export default ThemeExplore;
